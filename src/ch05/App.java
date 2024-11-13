7319f936f3f00fc57b29151981e8639ace4456e8
lhs <fantasytotal@naver.com>
2024-11-13 14:08:59 +0900

    템플릿메서드패턴

---
+++ src/ch05/App.java
@@ -0,0 +1,22 @@
+package ch05;
+
+import ch05.teacher.FlutterTeacher;
+import ch05.teacher.HTMLTeacher;
+import ch05.teacher.JavaTeacher;
+import ch05.teacher.PythonTeacher;
+
+public class App {
+    public static void main(String[] args) {
+        JavaTeacher t1 = new JavaTeacher();
+        t1.수업하기();
+
+        PythonTeacher t2 = new PythonTeacher();
+        t2.수업하기();
+
+        HTMLTeacher t3 = new HTMLTeacher();
+        t3.수업하기();
+
+        FlutterTeacher t4 = new FlutterTeacher();
+        t4.수업하기();
+    }
+}
